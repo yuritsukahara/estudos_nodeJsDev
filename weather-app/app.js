59e6df0b757eeac000fd3d262a77bab8060c11fe
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-08-12 23:29:28 -0300

    error handling check! 👌

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,38 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const weatherKey = process.env.WEATHERAPIKEY
-const mapBoxKey = process.env.MAPBOXAPIKEY
+const weatherKey = process.env.WEATHERAPIKEY;
+const mapBoxKey = process.env.MAPBOXAPIKEY;
 
 const request = require('postman-request');
 
-const url = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=Apucarana`
+const url = `http://api.weatherstack.com/current?access_key=${weatherKey}&query=Apucarana`;
 
-request({ url: url, json: true}, (error, response) =>{
-    const currentData = response.body.current
-    console.log(`Agora: ${currentData.temperature}ºC. Com a sensação de ${currentData.feelslike}ºC.`)
- })
+request({ url: url, json: true }, (error, response) => {
+	if (error) {
+		console.log('Unable to connect to weather service!');
+	} else if (response.body.error) {
+		console.log('Unable to find location ');
+	} else {
+		const currentData = response.body.current;
+		console.log(
+			`Agora: ${currentData.temperature}ºC. Com a sensação de ${currentData.feelslike}ºC.`
+		);
+	}
+});
 
+// Geocoding
 
-const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${mapBoxKey}`
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${mapBoxKey}`;
 
- request({ url: mapBoxUrl, json: true}, (error, response) =>{
-    const mapBoxData = response.body.features[0]
-    const latitude = mapBoxData.center[0]
-    const longitude = mapBoxData.center[1]
-    console.log('latitude:' + latitude + '\n' + 'longitude:' + longitude)
- } )
+request({ url: geocodeURL, json: true }, (error, response) => {
+	if (error) {
+		console.log('Unable to connect to geocode service!');
+	} else if (response.body.error) {
+		console.log('Unable to complete geocode request');
+	} else {
+		const geoData = response.body.features[0];
+		const latitude = geoData.center[0];
+		const longitude = geoData.center[1];
+		console.log('latitude:' + latitude + '\n' + 'longitude:' + longitude);
+	}
+});
