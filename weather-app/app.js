696db492fb29bb3db42e803ff7ec963cbd460107
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-08-18 00:23:39 -0300

    cli command

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,23 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+const args = process.argv.slice(2);
 
-geocode('Apucarana', (error, data) => {
-	console.log('Error', error);
-	console.log('Data', data);
-	forecast(data.latitude, data.longitude, (error, data) => {
-		console.log('Error', error);
-		console.log('Data', data);
+const locationName = args[0];
+
+if (!locationName) {
+	return console.log('Digite uma cidade');
+}
+
+geocode(locationName, (error, data) => {
+	if (error) {
+		return console.log(error);
+	}
+
+	forecast(data.latitude, data.longitude, (error, forecastData) => {
+		if (error) {
+			return console.log(error);
+		}
+		console.log(data.location);
+		console.log(forecastData);
 	});
-	
 });
-
