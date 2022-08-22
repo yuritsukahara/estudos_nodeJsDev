c742f3d0aee0d1b416eb7e2c348ac7f4a2197739
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-08-22 20:44:00 -0300

    handlebars study

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,35 @@
 const path = require('path');
-
 const express = require('express');
 
 const app = express();
-
 const publicDirPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates')
+
 app.use(express.static(publicDirPath));
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+
+app.get('', (req, res) =>{
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Yuri Tsukahara',
+	})
+})
+
+app.get('/about', (req, res)=>{
+	res.render('about', {
+		title:'About Me',
+		name:'Yuri Tsukahara',
+	})
+})
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		helpMessage:'Help Message'
+
+	})
+})
 
 app.get('/weather', (req, res) => {
 	res.send({ location: 'location', forecast: 'forecast' });
