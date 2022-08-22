2c6f1bff84cfabbc0f82a4df3bbb8d071b89647c
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-08-21 21:15:28 -0300

    setup nodemon

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,22 +3,31 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello express!');
+	res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-	res.send('Help page');
+	res.send([
+		{
+			name: 'Yuri',
+			age: 29,
+		},
+		{
+			name: 'Ju',
+			age: 23,
+		},
+	]);
 });
 
 app.get('/about', (req, res) => {
-	res.send('About page');
+	res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-	res.send('Your weather');
+	res.send({ location: 'location', forecast: 'forecast' });
 });
 
 app.listen(3000, () => {
 	console.log('Server is up on port 3000.');
-	console.log('htpp://localhost:3000');
+	console.log('http://localhost:3000');
 });
