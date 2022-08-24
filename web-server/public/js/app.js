6b400f0a0ec053baa4a7d04f819839a8fd82fb67
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-08-23 22:42:25 -0300

    fetch data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
-console.log('Client side js loaded')
+console.log('Client side js loaded');
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+fetch('http://localhost:3000/weather?address=asd123').then((response) => {
+	response.json().then((data) => {
+		if (data.error) {
+			return console.log(data.error);
+		}
+
+		console.log(data.location, data.forecast);
+	});
+});
