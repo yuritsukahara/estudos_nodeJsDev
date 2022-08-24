f90790834f4841776d972c8097baa54fa0780653
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-08-24 19:39:32 -0300

    Event listener form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,12 +6,23 @@ console.log('Client side js loaded');
 //     })
 // })
 
-fetch('http://localhost:3000/weather?address=asd123').then((response) => {
-	response.json().then((data) => {
-		if (data.error) {
-			return console.log(data.error);
-		}
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
 
-		console.log(data.location, data.forecast);
-	});
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const location = search.value;
+
+	fetch('http://localhost:3000/weather?address=' + location).then(
+		(response) => {
+			response.json().then((data) => {
+				if (data.error) {
+					return console.log(data.error);
+				}
+
+				console.log(data.location, data.forecast);
+			});
+		}
+	);
 });
