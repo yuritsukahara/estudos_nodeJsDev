fa447437a8970b30d19325041accde8d0b705aae
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-10-10 21:06:20 -0300

    updating endpoints

--- taskmanager/src/index.js
+++ taskmanager/src/index.js
@@ -8,78 +8,98 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
 	const user = new User(req.body);
 
-	user.save()
-		.then(() => {
-			res.status(201).send(user);
-		})
-		.catch((e) => {
-			res.status(400).send(e);
-		});
+	try {
+		await user.save();
+		res.status(201).send(user);
+	} catch (e) {
+		res.status(400).send(e);
+	}
 });
 
-app.get('/users', (req, res) => {
-	User.find({})
-		.then((users) => {
-			res.send(users);
-		})
-		.catch((e) => {
-			res.status(500).send();
-		});
+app.get('/users', async (req, res) => {
+	try {
+		const users = await User.find({});
+		res.send(users);
+	} catch (e) {
+		res.status(500).send();
+	}
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
 	const _id = req.params.id;
 
-	User.findById(_id)
-		.then((user) => {
-			if (!user) {
-				return res.status(404).send();
-			}
-			res.send(user);
-		})
-		.catch((e) => {
-			res.status(500).send();
-		});
+	try {
+		const user = await User.findById(_id);
+		if (!user) {
+			return res.status(404).send();
+		}
+		res.send(user);
+	} catch (e) {
+		res.status(500).send();
+	}
 });
 
-app.post('/tasks', (req, res) => {
+app.patch('/users/:id', async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ['name', 'email', 'password', 'age'];
+	const isValidOperation = updates.every((update) =>
+		allowedUpdates.includes(update)
+	);
+
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates!' });
+	}
+
+	try {
+		const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!user) {
+			return res.status(404).send();
+		}
+
+		res.send(user);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+app.post('/task', async (req, res) => {
 	const task = new Task(req.body);
-
-	task.save()
-		.then(() => {
-			res.status(201).send(task);
-		})
-		.catch((e) => {
-			res.status(400).send(e);
-		});
+	try {
+		await task.save();
+		res.status(201).send(task);
+	} catch (e) {
+		res.status(400).send(e);
+	}
 });
 
-app.get('/tasks', (req, res) => {
-	Task.find({})
-		.then((tasks) => {
-			res.send(tasks);
-		})
-		.catch((e) => {
-			res.status(500).send();
-		});
+app.get('/tasks', async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.send(tasks);
+	} catch (e) {
+		res.status(500).send();
+	}
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
 	const _id = req.params.id;
 
-	Task.findById(_id)
-		.then((task) => {
-			if (!task) {
-				return res.status(404).send();
-			}
-			res.send(task);
-		})
-		.catch((e) => {
-			res.sendStatus(500).send();
-		});
+	try {
+		const task = await Task.findById(_id);
+		if (!task) {
+			return res.status(404).send();
+		}
+		res.send(task);
+	} catch (e) {
+		res.sendStatus(500).send();
+	}
 });
 
 app.listen(port, () => {
