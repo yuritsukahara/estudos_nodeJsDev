19602efd750cfae9d52406a895c799356ce36c69
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-10-18 19:23:42 -0300

    auth user setup

--- taskmanager/src/routers/user.js
+++ taskmanager/src/routers/user.js
@@ -32,13 +32,8 @@ router.post('/users/login', async (req, res) => {
 	}
 });
 
-router.get('/users', auth, async (req, res) => {
-	try {
-		const users = await User.find({});
-		res.send(users);
-	} catch (e) {
-		res.status(500).send();
-	}
+router.get('/users/me', auth, async (req, res) => {
+	res.send(req.user);
 });
 
 router.get('/users/:id', auth, async (req, res) => {
