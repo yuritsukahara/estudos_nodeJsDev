99a102727498b2dacd740705ea731e5bb7dc0c95
Yuri Tsukahara <yuritsukahara@outlook.com>
2022-10-22 13:05:50 -0300

    user time stamp

--- taskmanager/src/models/user.js
+++ taskmanager/src/models/user.js
@@ -4,54 +4,59 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Task = require('./task');
 
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	age: {
-		type: Number,
-		trim: true,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error('Age must be a positive number');
-			}
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
 		},
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		unique: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error('Email is invalid');
-			}
-		},
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 7,
-		trim: true,
-		validate(value) {
-			if (value.includes('password')) {
-				throw new Error('Password must not contain password');
-			}
-		},
-	},
-	tokens: [
-		{
-			token: {
-				type: String,
-				required: true,
+		age: {
+			type: Number,
+			trim: true,
+			default: 0,
+			validate(value) {
+				if (value < 0) {
+					throw new Error('Age must be a positive number');
+				}
 			},
 		},
-	],
-});
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+			lowercase: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error('Email is invalid');
+				}
+			},
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 7,
+			trim: true,
+			validate(value) {
+				if (value.includes('password')) {
+					throw new Error('Password must not contain password');
+				}
+			},
+		},
+		tokens: [
+			{
+				token: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
 
 userSchema.methods.generateAuthToken = async function () {
 	const user = this;
